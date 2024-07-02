fad239275430f8216432ebfe93912c9fb988d4e0
Henric Ryden <henric.ryden@neuroradkarolinska.se>
2024-07-02 15:56:04 +0200

    instantiate std::complex<float>

--- src/recon/advancedRecon.cpp
+++ src/recon/advancedRecon.cpp
@@ -8,3 +8,7 @@ void modifyTensor(Eigen::Tensor<T, rank>& tensor, const T val) {
 
 template void modifyTensor<float, 3>(Eigen::Tensor<float, 3>& tensor, float val);
 template void modifyTensor<float, 4>(Eigen::Tensor<float, 4>& tensor, float val);
+
+using Scalar = std::complex<float>;
+template void modifyTensor<CplxScalar, 3>(Eigen::Tensor<CplxScalar, 3>& tensor, CplxScalar val);
+template void modifyTensor<CplxScalar, 4>(Eigen::Tensor<CplxScalar, 4>& tensor, CplxScalar val);
