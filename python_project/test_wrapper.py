c3090303c7f8924e17f157d0fa1a6354a164f698
Henric Ryden <henric.ryden@neuroradkarolinska.se>
2024-07-02 19:50:49 +0200

    example usage

--- python_project/test_wrapper.py
+++ python_project/test_wrapper.py
@@ -9,5 +9,17 @@ def include_extern() -> None:
 
 # Sets the path
 include_extern()
+
+# Now we can import it
 import extern
-print(extern)
+
+array = np.arange(3 * 4 * 1, dtype=np.float32).reshape(3, 4, 1)
+print('Numpy array input:\n', array.reshape(3,4))
+out1 = extern.recon.fancyFunction(array, -1)
+print('Numpy array output:\n', out1.reshape(3,4))
+
+print('\n\nFortran layout (should be the same as above):')
+array_copy = np.ndarray.copy(array, order='F')
+print(array_copy.reshape(3,4))
+out2 = extern.recon.fancyFunction(array_copy, -2)
+print('Fortran numpy array output (should be the same as above):\n', out2.reshape(3,4))
